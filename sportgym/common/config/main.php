35e5052dfa260dcd7be23e6561be6237c1c5f13b
Ricardo Marques <ricardofm1712@gmail.com>
2019-11-20 13:38:07 +0000

    Adicionar Pretty URLs

--- sportgym/common/config/main.php
+++ sportgym/common/config/main.php
@@ -12,5 +12,17 @@ return [
         'authManager' => [
             'class' => 'yii\rbac\DbManager',
         ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                    '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                    '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                    '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ),
+        ],
     ],
 ];
